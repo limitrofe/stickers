ca1436cfba5a26a659996d80359bd308d330c372
limitrofe <limitrofe@gmail.com>
2026-02-18 23:04:59 -0300

    Fix: Remove unstable single-process flag

--- index.js
+++ index.js
@@ -35,8 +35,6 @@ const client = new Client({
             '--disable-dev-shm-usage',
             '--disable-accelerated-2d-canvas',
             '--no-first-run',
-            '--no-zygote',
-            '--single-process', 
             '--disable-gpu'
         ],
         headless: true
